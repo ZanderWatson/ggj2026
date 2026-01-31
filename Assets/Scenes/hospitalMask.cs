e5178d830b97e176ebb5c196d41e2b9be6a084e5
Ethan Le <enathanle@gmail.com>
2026-01-31 13:55:11 -0800

    Added hospitalMask.cs with Hospital Mask's ability: restore 20 HP.

---
+++ Assets/Scenes/hospitalMask.cs
@@ -0,0 +1,57 @@
+// Ethan Le (1/31/2026): hospitalMask.cs
+// Allows player to use the Hospital Mask's ability: restores HP. 
+using System.Collections;
+using UnityEngine;
+
+public class HospitalMask : MonoBehaviour
+{
+    [Header("Ability Settings")]
+    public float cooldownTime = 10f;
+    public float hpRestore = 20f; // Amount of HP to restore.
+
+    [Header("Activation Key")]
+    public KeyCode abilityKey = KeyCode.E; // In case players do not want to use the mouse. 
+    public bool useMouseButton = false; // Toggle for mouse input. 
+
+    private bool isOnCooldown = false;
+    private player player;
+
+    public GameObject maskObject;
+
+    void Start()
+    {
+        player = GetComponent<player>(); // Get the player.cs script component. 
+    }
+
+    void Update()
+    {
+        if (!isOnCooldown) // Ability is ready to use. 
+        {
+            if (useMouseButton && Input.GetMouseButtonDown(1)) // Right mouse click to activate ability. 
+            {
+                StartCoroutine(ActivateHospitalMask()); // Start ability duration routine. 
+            }
+
+            else if (!useMouseButton && Input.GetKeyDown(abilityKey)) // Keyboard key alternative to activate ability. 
+            {
+                StartCoroutine(ActivateHospitalMask()); // Start ability duration routine. 
+            }
+        }
+    }
+
+    private IEnumerator ActivateHospitalMask()
+    {
+        isOnCooldown = true; // Set cooldown flag for ability. 
+
+        // Enable mask effects: 
+        player.health += hpRestore; // Restore the player's health. 
+
+        // Wait for duration of invincibility before continuing code execution: 
+        yield return new WaitForSeconds(abilityDuration); // Pauses code execution for set duration. 
+
+        // Start cooldown timer before ability can be used again:: 
+        yield return new WaitForSeconds(cooldownTime); // Pauses code execution for cooldown duration. 
+
+        isOnCooldown = false; // Allow ability to be used again. 
+    }
+}
