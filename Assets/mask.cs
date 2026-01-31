2e2572c8d80302b84f1a709d4c2027d0b3020ea2
ZanderWatson <weirdmushroom73@gmail.com>
2026-01-30 18:49:03 -0800

    player

---
+++ Assets/mask.cs
@@ -0,0 +1,38 @@
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class mask : MonoBehaviour
+{
+    public GameObject character;
+    Rigidbody2D rb;
+    bool followingPlayer = false;
+    int maskType;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        character = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+        maskType = Random.Range(1, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(character.transform.position, transform.position) < 1.5f)
+        {
+            followingPlayer = true;
+        } 
+        else if (Vector2.Distance(character.transform.position, transform.position) < 0.3f)
+        {
+            player.masks.Add(maskType);
+            Destroy(gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (followingPlayer)
+        {
+            Vector3.MoveTowards(transform.position, character.transform.position, 0.01f);
+        }
+    }
+}
--- Assets/player.cs
+++ Assets/player.cs
@@ -1,16 +1,63 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public GameObject projectile;
+    [NonSerialized] public float speed;
+    float projSpeed = 10;
+    const float MAX_SPEED = 10;
+    public static ArrayList masks = new ArrayList();
+    float attackSpeedTimer;
+
+    bool up; bool left; bool right; bool down;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        speed = 8;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
+        up = Input.GetKey(KeyCode.W);
+        left = Input.GetKey(KeyCode.A);
+        down = Input.GetKey(KeyCode.S);
+        right = Input.GetKey(KeyCode.D);
+        // Ability
+        if (Input.GetMouseButtonDown(0)) {
+            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity);
+            proj.SetActive(true);
+            proj.GetComponent<Rigidbody2D>().AddForce((mouse - new Vector2(transform.position.x, transform.position.y)).normalized * projSpeed, ForceMode2D.Impulse);
+            Destroy(proj, 3);
+        }
+    }
+    private void FixedUpdate()
+    {
+        // Movement
+        if (up)
+        {
+            rb.AddForceY(speed, ForceMode2D.Force);
+        }
+        if (left)
+        {
+            rb.AddForceX(-speed, ForceMode2D.Force);
+        }
+        if (down)
+        {
+            rb.AddForceY(-speed, ForceMode2D.Force);
+        }
+        if (right)
+        {
+            rb.AddForceX(speed, ForceMode2D.Force);
+        }
+        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, MAX_SPEED);
         
     }
 }
