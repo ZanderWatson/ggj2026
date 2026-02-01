167c4543c7ef94de01088c8ccf25e0fca90cc214
ZanderWatson <weirdmushroom73@gmail.com>
2026-02-01 11:19:32 -0800

    Main menu info

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,8 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject options;
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    public void ToggleOptionsScreen()
+    {
+        options.SetActive(!options.activeSelf);
     }
 }
